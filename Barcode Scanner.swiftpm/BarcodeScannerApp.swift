285e58a02efd91474ff34c287ccc59ad05314f84
Will DeBerry <willdeberry@gmail.com>
2021-12-28 12:55:52 -0500

    Add barcode scanner playground

---
+++ Barcode Scanner.swiftpm/BarcodeScannerApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct BarcodeScanner: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ Barcode Scanner.swiftpm/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    @StateObject var viewModel = BarcodeScannerViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScannerView(scannedCode: $viewModel.scannedCode, 
+                            alertItem: $viewModel.alertItem)
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                Spacer().frame(height: 60)
+                Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                Text(viewModel.statusText)
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(viewModel.statusTextColor)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+            .alert(item: $viewModel.alertItem) { alertItem in
+                Alert(title: Text(alertItem.title), 
+                      message: Text(alertItem.message), 
+                      dismissButton: alertItem.dismissButton)
+            }
+        }
+    }
+}
