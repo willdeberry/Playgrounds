8032e122e4bd1206057b84f224ed0d56997610d0
Will DeBerry <willdeberry@gmail.com>
2021-12-27 20:26:50 -0500

    Initial Commit

---
+++ WeatherApp.swiftpm/WeatherButton.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold))
+            .cornerRadius(10)
+    }
+}
